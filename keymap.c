8de771e171c7b162f2c34c40768998c372731323
WandersFar <WandersFar@users.noreply.github.com>
2025-05-06 00:30:31 -0400

    Add files via upload

--- keymap.c
+++ keymap.c
@@ -651,9 +651,9 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
     case TD(LEFT):
     case TD(RIGHT):
     case TD(PGUP):
-    case TD(PGDN): return 200;
+    case TD(PGDN):
     case TD(HOME):
-    case TD(END): return 320;
+    case TD(END): return 200;
     default: return TAPPING_TERM; } }
 
 td_state_t cur_dance(tap_dance_state_t *state) {
@@ -753,7 +753,8 @@ static uint16_t recent = KC_NO;
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
     case DF(1): if (record->event.pressed) { defer_exec(3000, callback, NULL); } return true;
-    case DF(0): if (record->event.pressed) { defer_exec(500, callback, NULL); } return true;
+    case LT(2,KC_0): if (record->tap.count && record->event.pressed)
+      { set_single_default_layer(0); defer_exec(500, callback, NULL); } return true;
     case CURLY:
       static uint16_t TIMER;
       if (record->event.pressed) { TIMER = timer_read(); }
@@ -775,7 +776,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [1] = LAYOUT_ortho_4x12(KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DEL,
     KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
     KC_QUOT, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SPC, KC_ENT,
-    LT(2,KC_MUTE), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, DF(0)),
+    LT(2,KC_MUTE), KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, LT(2,KC_0)),
   [2] = LAYOUT_ortho_4x12(KC_EQL, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, TD(DASH),
     KC_LBRC, C(KC_PPLS), A(KC_F4), KC_MS_U, KC_BTN1, KC_BTN3, KC_BTN3, KC_BTN1, KC_MS_U, RCS(KC_ESC), G(KC_PSCR), KC_RBRC,
     KC_DQT, KC_TILD, KC_MS_L, KC_MS_D, KC_MS_R, KC_BTN2, KC_BTN2, KC_MS_L, KC_MS_D, KC_MS_R, KC_COLN, KC_BSLS,
